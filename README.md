b8b720010496bd2bac132395649b69c6f349c361
sleroy <sylvain.leroy@tocea.com>
2017-03-28 07:39:46 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # hexo-generator-slideshare
-Hexo Generator for Slideshare
+
+<a href="https://codeclimate.com/github/sleroy/hexo-generator-slideshare"><img src="https://codeclimate.com/github/sleroy/hexo-generator-slideshare/badges/gpa.svg" /></a>
+
+Generates a slideshare frame inside your posts.
+
+This package is still pretty rough around the edges and is not recommended for anything important yet. Plenty of code cleanup and tests to be done. Suggestions, contributions, or bug reports gratefully received.
+
+
+## Install
+
+``` bash
+$ npm install hexo-generator-slideshare --save
+```
+
+### 0.&
+- First version
---
+++ index.js
@@ -0,0 +1,49 @@
+/**
+ * hexo-generator-slideshare
+ * https://github.com/sleroy/hexo-generator-slideshare
+ * Copyright (c) 2017, Sylvain Leroy
+ * Licensed under the Apache license 2.
+ *
+ * Syntax:
+ * {% slideshare [video_id] %} or {% [video_id [width [height]]] %}
+ */
+
+hexo.extend.tag.register('slieshare', function(args) {
+    var base = '//www.slideshare.net/slideshow/embed_code/key/',
+        video_id = args[0],
+        width = args[1],
+        height = args[2],
+        frameborder = args[3],
+        marginwidth = args[4],
+        marginheight = args[5],
+        style = "border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;",
+        allowfullscreen = args[6];
+
+    var config = hexo.config.slideshare || {};
+
+    width = config.width || 510;
+    height = config.height || 400;
+
+    var tag = '<iframe src="' + base + video_id + '" frameborder=' + frameborder + ' marginwidth=' + marginwidth + ' marginheight=' + marginheight + ' width=' + width + ' height=' + height + ' scrolling="no" style="' + style + '"> </iframe>';
+
+    return tag;
+
+    /**
+     *
+     * < iframe src = "//www.slideshare.net/slideshow/embed_code/key/qtGS6vJHCYXQ80"
+     width = "595"
+     height = "485"
+     frameborder = "0"
+     marginwidth = "0"
+     marginheight = "0"
+     scrolling = "no"
+     style = "border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;"
+     allowfullscreen > < /iframe>
+     
+     <div style="margin-bottom:5px"> <strong> <a href="/ / www.slideshare.net / SylvainLeroy / rappels - modularisation - application - cc " title="
+     Rappels Modularisation application C / C++" target="
+     _blank ">Rappels Modularisation application C/C++</a> </strong> from <strong><a target="
+     _blank " href=" //www.slideshare.net/SylvainLeroy">Sylvain Leroy</a></strong> </div>
+     *
+     */
+});
